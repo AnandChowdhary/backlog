2a244e96462f940a06adbfb2d2f59337d7a69ceb
Anand Chowdhary <mail@anandchowdhary.com>
2020-06-17 22:13:59 +0530

    :recycle: Use specific deno.land/std version

--- mod.ts
+++ mod.ts
@@ -1,10 +1,10 @@
-import { join } from "https://deno.land/std/path/mod.ts";
+import { join } from "https://deno.land/std@0.52.0/path/mod.ts";
 import { timeAgo } from "https://deno.land/x/time_ago/mod.ts";
 import {
   readFileStr,
   writeFileStr,
   writeJson,
-} from "https://deno.land/std/fs/mod.ts";
+} from "https://deno.land/std@0.52.0/fs/mod.ts";
 
 const GH_PAT = Deno.env.get("GH_PAT");
 
